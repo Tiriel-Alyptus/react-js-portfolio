e4942f4c6f2c1ac1be37a363dd2e8b24137747f7
Tiriel-Alyptus <lorenzo.decaudin@gmail.com>
2022-04-25 12:35:30 +0200

    parallax+scroll+scroll-parallax

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'coline-state': '#264F8C',
+      },
+    },
+  },
+  plugins: [],
+
+  borderWidth: {
+    DEFAULT: '1px',
+    '0': '0',
+    '2': '2px',
+    '3': '3px',
+    '4': '4px',
+    '6': '6px',
+    '8': '200px',
+ }
+}
