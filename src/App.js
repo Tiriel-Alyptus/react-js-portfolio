1abc6ace6579024675b9da16ea39b21be5d345c2
Tiriel-Alyptus <lorenzo.decaudin@gmail.com>
2022-04-26 22:16:20 +0200

    section marche pas - mais triés

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './components/Section/App.css';
+import './index.js'
+import header from "./components/Section/footer/footer";
+import body from "./components/Section/body/body";
+import footer from "./components/Section/footer/footer";
+import Fade from "react-reveal/Fade";
+import { Parallax } from 'react-parallax';
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+     <div className="App">
+        <header header/>
+        <body body/>
+        <footer footer/>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
